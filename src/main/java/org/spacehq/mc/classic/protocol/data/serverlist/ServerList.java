bb61d38a648d9266b68f942cf27c3bddbb5a77c6
Steven Smith <Steveice10@gmail.com>
2015-07-16 13:11:28 -0700

    Explicitly set cookie policy.

--- src/main/java/org/spacehq/mc/classic/protocol/data/serverlist/ServerList.java
+++ src/main/java/org/spacehq/mc/classic/protocol/data/serverlist/ServerList.java
@@ -2,14 +2,23 @@ package org.spacehq.mc.classic.protocol.data.serverlist;
 
 import org.spacehq.mc.classic.protocol.AuthenticationException;
 
-import java.io.*;
-import java.net.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.UnsupportedEncodingException;
+import java.net.CookieHandler;
+import java.net.CookieManager;
+import java.net.CookiePolicy;
+import java.net.URL;
+import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.HashMap;
 import java.util.Map;
 
 public class ServerList {
 	static {
-		CookieHandler.setDefault(new CookieManager());
+		CookieHandler.setDefault(new CookieManager(null, CookiePolicy.ACCEPT_ALL));
 	}
 
 	public static void login(String username, String password) throws AuthenticationException {
